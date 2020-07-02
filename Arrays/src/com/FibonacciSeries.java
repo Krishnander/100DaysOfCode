6cf783c043f844826466421416f5b7cbe2fdde82
Gopalakrishnan <gopalakrishnan@GK>
2020-07-02 18:32:24 +0530

    Fibonacci

--- Arrays/src/com/FibonacciSeries.java
+++ Arrays/src/com/FibonacciSeries.java
@@ -4,17 +4,35 @@ import java.util.Scanner;
 
 public class FibonacciSeries {
 
+	/*
+	 * public int getFibonacci(int n) { if(n<=1) { return n; } else {
+	 * 
+	 * return getFibonacci(n-2)+getFibonacci(n-1); } }
+	 */
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the input");
-		int n=sc.nextInt();
+		int n = sc.nextInt();
 		sc.close();
-		int i;
-		for (i = 0; i < n; i++) {
-			i+=i;
+
+		int firstTerm = 0, secondTerm = 1;
+		int thirdTerm = 0;
+		System.out.println(firstTerm);
+		System.out.println(secondTerm);
+		for (int i = n - 2; i != 0; i--) {
+			thirdTerm = secondTerm + firstTerm;
+			firstTerm = secondTerm;
+			secondTerm = thirdTerm;
+			System.out.println(thirdTerm);
 		}
-		System.out.println("The fibonacci series of first "+n+" numbers is "+n);
+
+		/*
+		 * FibonacciSeries fSeries = new FibonacciSeries();
+		 * System.out.println(fSeries.getFibonacci(8));
+		 */
+
 	}
 
 }
