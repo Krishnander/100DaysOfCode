a12f87884df4bc273ec5878e942e58dd40dc56a3
Gopalakrishnan <gopalakrishnan@GK>
2020-07-02 17:42:17 +0530

    Changes

--- Arrays/src/com/FibonacciSeries.java
+++ Arrays/src/com/FibonacciSeries.java
@@ -1,10 +1,20 @@
 package com;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the input");
+		int n=sc.nextInt();
+		sc.close();
+		int i;
+		for (i = 0; i < n; i++) {
+			i+=i;
+		}
+		System.out.println("The fibonacci series of first "+n+" numbers is "+n);
 	}
 
 }
