dde5824dc4c772235750d56c09a357d579403004
Gopalakrishnan <gopalakrishnan@GK>
2020-07-03 00:21:09 +0530

    Palindrome

---
+++ Arrays/src/com/NumberPalindrome.java
@@ -0,0 +1,28 @@
+package com;
+
+import java.util.Scanner;
+
+public class NumberPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the input");
+		int num = sc.nextInt();
+		sc.close();
+		int n=num;
+		int rev=0;
+		 while(n != 0) {
+	            int digit = n % 10;
+	            rev = rev * 10 + digit;
+	            n /= 10;
+	        }
+		if (rev==num) {
+			System.out.println(num+ " is a palindrome");
+		}
+		else {
+			System.out.println(num+" is not a palindrome");
+		}
+	}
+
+}
