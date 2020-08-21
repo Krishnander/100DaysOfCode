d102485299617fe0ba5a04f9bc85971b8c23a804
Krishnander <gopi20315@gmail.com>
2020-08-22 04:14:22 +0530

    Changes

--- LinkedList/com/LinkedList.java
+++ LinkedList/com/LinkedList.java
@@ -4,83 +4,155 @@ public class LinkedList<E> {
 	Node headNode;
 	Node lastNode;
 	int currentSize;
+
 	class Node {
-		int data;
+		E data;
 		Node next;
 
-		Node(int d) {
+		Node(E d) {
 			data = d;
 			next = null;
 		}
 	}
+
 	public LinkedList() {
-		headNode=null;
-		currentSize=0;
+		headNode = null;
+		currentSize = 0;
 	}
-	public void addFirst(int new_data){
-		Node new_Node=new Node(new_data);
-		if (headNode==null) {
-			headNode=new_Node;
+
+	public void addFirst(E new_data) {
+		Node new_Node = new Node(new_data);
+		if (headNode == null) {
+			headNode = new_Node;
 			return;
 		}
-		new_Node.next=headNode;
-		headNode=new_Node;
+		new_Node.next = headNode;
+		headNode = new_Node;
 		currentSize++;
 	}
-	
-	public void addLast(int new_data) {
-		Node new_Node=new Node(new_data);
-		if (headNode==null) {
-			headNode=lastNode=new_Node;
+
+	public void addLast(E new_data) {
+		Node new_Node = new Node(new_data);
+		if (headNode == null) {
+			headNode = lastNode = new_Node;
 			return;
 		}
-		while (headNode.next!=null) {
-			headNode.next=new_Node;
+		while (headNode.next != null) {
+			headNode.next = new_Node;
 		}
-		lastNode.next=new_Node;
-		lastNode=new_Node;
+		lastNode.next = new_Node;
+		lastNode = new_Node;
 		return;
 	}
-	
+
 	public void removeFirst() {
-		if (headNode==null) {
+		if (headNode == null) {
 			System.out.println("List is empty");
+			return;
 		}
-		int temp=headNode.data;
-		if (headNode==lastNode) {
-			headNode=lastNode=null;
+
+		Node temp = headNode;
+		while (temp.next != null) {
+			temp = temp.next;
 		}
-		else {
-			headNode=headNode.next;
+		lastNode = temp;
+
+		if (headNode == lastNode) {
+			headNode = lastNode = null;
+		} else {
+			headNode = headNode.next;
 		}
 		currentSize--;
-		System.out.println("The number removed is "+temp);
-		
 	}
-	
+
 	public void removeLast() {
-		if (headNode==null) {
+		if (headNode == null) {
 			System.out.println("List is empty");
+			return;
 		}
-		if (headNode==lastNode) {
+		Node temp = headNode;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		lastNode = temp;
+		if (headNode == lastNode) {
 			removeFirst();
+			return;
 		}
-		else {
-			Node tempNode=headNode;
-			while (tempNode.next!=null) {
-				tempNode.next=tempNode;
-			}
-			lastNode=tempNode;
-			lastNode.next=null;
+		Node currNode = headNode;
+		Node preNode = null;
+		while (currNode != lastNode) {
+			preNode = currNode;
+			currNode = currNode.next;
 		}
+		preNode.next = null;
+		lastNode = preNode;
+		System.out.println("The number removed is " + currNode.data);
+
 	}
-	
-	/*
-	 * public E removeAtGivenPosition(E obj) { Node currNode=headNode; Node
-	 * preNode=null; while (currNode!=null) { E o=E(currNode.data); if
-	 * (((Comparable<E>)obj).compareTo(o)) {
-	 * 
-	 * } } }
-	 */
-	
+
+	@SuppressWarnings("unchecked")
+	public E removeGivenElement(E obj) {
+		Node currNode = headNode;
+		Node preNode = null;
+		while (currNode != null) {
+			if (((Comparable<E>) obj).compareTo(currNode.data) == 0) {
+				if (currNode == headNode) {
+					removeFirst();
+				}
+				if (currNode == lastNode) {
+					removeLast();
+				}
+				currentSize--;
+				preNode.next = currNode.next;
+				return currNode.data;
+			}
+			preNode = currNode;
+			currNode = currNode.next;
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public boolean contains(E obj) {
+		Node currNode = headNode;
+		while (currNode != null) {
+			if (((Comparable<E>) obj).compareTo(currNode.data) == 0) {
+				System.out.println("List contains " + obj);
+				return true;
+			}
+			currNode = currNode.next;
+		}
+		System.out.println("Not available");
+		return false;
+	}
+
+	public void display() { // Node current will point to head
+		Node current = headNode;
+
+		if (headNode == null) {
+			System.out.println("List is empty");
+			return;
+		} //
+		System.out.println("Nodes of singly linked list: ");
+		while (current != null) { // Prints each node by incrementing pointer
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		LinkedList<Integer> testList = new LinkedList<Integer>();
+		int n = 10;
+		for (int i = 0; i < n; i++) {
+			testList.addFirst(i);
+		}
+		testList.removeFirst();
+		testList.removeLast();
+		testList.removeGivenElement(6);
+		testList.contains(8);
+		testList.display();
+	}
+
 }
